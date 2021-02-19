3fab46240ff73ed6308521d26222aec471a95585
lynn-learn <77292354+lynn-learn@users.noreply.github.com>
2021-02-19 08:05:09 -0500

    Add files via upload

---
+++ archivo 1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 07:33:51 2021
+
+@author: Derly garzon
+"""
+
+from archivo2 import *
+
+funcion()
+
+funcion2()
---
+++ archivo2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 19 07:33:48 2021
+
+@author: Derly garzon
+"""
+
+def funcion():
+    print(123)
+    
+    
+def funcion2():
+    print(12355555)    
